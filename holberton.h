5c7fdb2bec0be4eb26709340659380619e2cda98
felipe <felipe_londono1@hotmail.com>
2019-10-28 22:06:55 +0000

    [FEATURE]added new prototype

--- holberton.h
+++ holberton.h
@@ -1,12 +1,15 @@
 #ifndef HOLBERTON_
 #define HOLBERTON_
-
+#include <stdlib.h>
+#include <stdarg.h>
 int _printf(const char *format, ...);
+int (*structure(char letter))(va_list);
 
-struct func
+typedef struct func
 {
 	char *fmt;
-	void (*format)(char *);
+	int (*f)(va_list arg);
 }holder;
-
+int _chars(va_list arg);
+int _str(va_list arg);
 #endif
