90ee270f6e22b9440618b8b9f53744141908546e
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-28 14:49:34 +0000

    [FEATURE] test to print only one char

--- _printf.c
+++ _printf.c
@@ -11,18 +11,27 @@
  */
 int _printf(const char *format, ...)
 {
-	int str;
+	holder agmt[] = {
+		{"c", _chars},
+		{NULL, NULL}
+	};
+
+	int i, n;
 	va_list arg;
 
+	i = 0;
+	n = 0;
 	va_start(arg, format);
 
-	holder format[] = {
-		{"c", _chars}
-		{"s", _strs}
+	while (format && format[i])
+	{
+		if (format[i] == '%' && format[i + 1] != '%'
+		    && format[i + 1] == *(agmt[0].fmt))
+		{
+			n += agmt[0].f(arg);
+		}
+		i++;
 	}
-
-
 	va_end (arg);
-
-	return(str);
+	return (n);
 }
