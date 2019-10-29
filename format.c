98e13c1921ea9242b32f3e5b648669f58aabe48d
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-29 00:38:04 +0000

    [FEATURE] add print string function

--- format.c
+++ format.c
@@ -4,12 +4,31 @@
 #include <unistd.h>
 #include <stdarg.h>
 /**
- * chars - prints to the stdout a char
- * @c: character
+ *_chars - prints to the stdout a char
+ *@args: argument
+ *Return: a char
  */
 int _chars(va_list args)
 {
 	char c;
+
 	c = va_arg(args, int);
 	return (putchar(c));
+}
+/**
+ *_str - prints string
+ *@args: argument
+ *Return: a string
+ */
+int _str(va_list args)
+{
+	int i = 0;
+	char *str = va_arg(args, int);
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	return (0);
 }
