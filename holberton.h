99db62b2ba506ef750fa4458c9d5f80643660f0e
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-29 00:51:07 +0000

    [FIX] Betty style fix

--- holberton.h
+++ holberton.h
@@ -4,12 +4,15 @@
 #include <stdarg.h>
 int _printf(const char *format, ...);
 int (*structure(char letter))(va_list);
-
+/**
+ *func - structure for arguments
+ */
 typedef struct func
 {
 	char *fmt;
 	int (*f)(va_list arg);
-}holder;
+}
+holder;
 int _chars(va_list arg);
 int _str(va_list arg);
 #endif
